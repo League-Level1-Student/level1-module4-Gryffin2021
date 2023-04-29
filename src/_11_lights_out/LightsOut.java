ade21ce23be665ffc44603352f22f4d1eb02b770
league <league@192.168.1.81>
2023-04-28 18:27:34 -0700

    Lights out, simon says

--- src/_11_lights_out/LightsOut.java
+++ src/_11_lights_out/LightsOut.java
@@ -24,73 +24,76 @@ import javax.swing.JPanel;
  * 
  * 
  */
-public class LightsOut implements MouseListener{
+public class LightsOut implements MouseListener {
 
 	JPanel gamePanel = new JPanel();
 	JFrame frame = new JFrame();
 	public static JLabel[] array = new JLabel[25];
-	
+
 	public LightsOut() {
 
 		/** PART 1. CREATE YOUR LIGHT BOARD **/
-		//1. Make your gamePanel a 5x5 grid with setLayout(new GridLayout(5, 5));
+		// 1. Make your gamePanel a 5x5 grid with setLayout(new GridLayout(5, 5));
 		gamePanel.setLayout(new GridLayout(5, 5));
-			//2. Add 25 JLabels to your gamePanel (these are your lights)
-		
-			for(int i = 0; i < array.length; i ++) {
+		// 2. Add 25 JLabels to your gamePanel (these are your lights)
+
+		for (int i = 0; i < array.length; i++) {
 			array[i] = new JLabel();
 			array[i].setText("" + i);
-			Random ran = new Random(2);
+			Random ran = new Random();
 			int x = ran.nextInt(2);
-			if(x == 0) {
-			array[i].setBackground(Color.LIGHT_GRAY);
-			}else if(x == 1){
-			array[i].setBackground(Color.WHITE);
+			if (x == 0) {
+				array[i].setBackground(Color.LIGHT_GRAY);
+			} else if (x == 1) {
+				array[i].setBackground(Color.WHITE);
 			}
 			array[i].setOpaque(true);
 			array[i].addMouseListener(this);
 			gamePanel.add(array[i]);
-			}
-			
-			//3. Use setText() to add a position number to each light (0-24).
+		}
 
-			//4. Set the background of each light to LIGHT_GRAY
-			// - you will also have to set the background to opaque.
-			// - Use light.setOpaque(true);
+		// 3. Use setText() to add a position number to each light (0-24).
 
-			//5. Add a mouseListener to each light
-		
-		
-		//6. Add your panel to a frame
-frame.add(gamePanel);
-		//7. Set the size of the frame
-frame.setSize(3600,3600);
-frame.pack();
-frame.setVisible(true);
+		// 4. Set the background of each light to LIGHT_GRAY
+		// - you will also have to set the background to opaque.
+		// - Use light.setOpaque(true);
+
+		// 5. Add a mouseListener to each light
+
+		// 6. Add your panel to a frame
+		frame.add(gamePanel);
+		// 7. Set the size of the frame
+		frame.setSize(3600, 3600);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		/** PART 2: TOGGLE NEIGHBORING LIGHTS **/
 		// 1. Get the light that was clicked on `(JLabel) e.getSource`
-		JLabel label = (JLabel)(e.getSource());
+		JLabel label = (JLabel) (e.getSource());
 		// 2. Get the number (position) of the light
 		String position = label.getText();
-		 int position$ = Integer.parseInt(position);
+		int position$ = Integer.parseInt(position);
 		// 3. Now use the makeMove method to code which lights turn on and off.
 		makeMove(position$);
 		// 4.Check if the player has won (e.g. all the lights are off)
 		// ---- HINT: use `getLightAtPosition` to get the light at each position
 		// ---------- use 'getBackground' to get the light color
-		for(int i = 0; i < array.length; i ++) {
+		boolean hasWon = true;
+		for (int i = 0; i < array.length; i++) {
 			getLightAtPosition(i);
-			if(array[i].getBackground() == Color.LIGHT_GRAY) {
-				JOptionPane.showMessageDialog(null, "You Won!");
+			if (array[i].getBackground() != Color.LIGHT_GRAY) {
+				hasWon = false;
 			}
+		}
+		if(hasWon == true) {
+			JOptionPane.showMessageDialog(null, "You Won!");
 		}
 		/** PART 3: RANDOMIZE YOUR BOARD **/
 		// Now that your game works can you make the game start with some lights on?
-		
+
 	}
 
 	void makeMove(int pos) {
--- src/_99_extra/_02_simon_says/SimonSays.java
+++ src/_99_extra/_02_simon_says/SimonSays.java
@@ -10,7 +10,11 @@ import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import game_tools.Sound;
 
 public class SimonSays extends KeyAdapter {
     HashMap<Integer, String> images = new HashMap<Integer, String>();
@@ -21,71 +25,86 @@ public class SimonSays extends KeyAdapter {
 
     // Complete steps 1 - 7 before you test
     // 1. Declare a JFrame variable
-
+    JFrame frame = new JFrame();
     public void run() {
         // 2. Add the four images that match keyboard keys like this:
         // images.put(KeyEvent.VK_UP, "up.jpg");
-
+    	images.put(KeyEvent.VK_UP, "up.jpg");
+    	images.put(KeyEvent.VK_UP, "down.jpg");
+    	images.put(KeyEvent.VK_UP, "left.jpg");
+    	images.put(KeyEvent.VK_UP, "right.jpg");
         // 3. Use a JOptionPane to tell the user the rules: "Press the matching
         // key when
         // 'Simon says' otherwise press a different key"
-
+    	JOptionPane.showMessageDialog(null, "Press the matching when 'Simon says' otherwise press a different key");
         // 4. Call the showImage method to show an image
-
+    	showImage();
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         // 15. Make a points variable to track the score.
-
+    	int points = 0;
         // 16. If the keyCode matches the imageIndex and "Simon says"
-
+    	if(e.getKeyCode() == imageIndex && simonSays == true) {
         // 17. Increase the value of score
-
+    	points += 1;
         // 18. Use the Sound.speak method to tell the user they were correct
-
+    	Sound.speak("Correct");
+    	frame.dispose();
+    	showImage();
         // 19. If the keyCode doesn't match the imageIndex and "Simon didn't
         // say..."
-
-        // 20. Increase the value of score
-
-        // 21. Use the Sound.speak method to tell the user they were correct
-
+    	}else if(e.getKeyCode() == imageIndex && simonSays == false) {
+        // 20. Decrease the value of score
+    	points -= 1;
+        // 21. Use the Sound.speak method to tell the user they were incorrect
+    	Sound.speak("Incorrect");
+    	frame.dispose();
+    	showImage();
         // 22. Increment tries by 1
-
+    	tries += 1;
         // 25. If tries is greater than 9 (or however many you want)...
-
+    	}
+    	if(tries > 3) {
         // 26. Tell the user their score
-
+    	JOptionPane.showMessageDialog(null, "Your score was " + points);
         // 27. Exit the program
-
+    	System.exit(0);
         // 23. Dispose of the frame
-
-        // 24. Call the showImage method to show a new image
+    	
+    	}
     }
 
     private void showImage() {
         // 5. Initialize your frame to a new JFrame()
-
+    	JFrame frame = new JFrame();
         // 6. Set the frame to visible
-
+    	frame.setVisible(true);
         // 7. Uncomment the following line to add a random image to your frame
-        // frame.add(getNextRandomImage());
+        frame.add(getNextRandomImage());
 
         // 8. Set the name of your frame
-
+        frame.setName("Simon Says");
         // 9. Pack the frame
-
+        frame.pack();
         // 10. Set the defaultCloseOperation of your frame to
         // JFrame.EXIT_ON_CLOSE
-
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         // 11. Add a key listener to the frame
-
+        frame.addKeyListener(this);
         // 12. Create a new instance of Random
-
+        Random ran = new Random();
         // 13. Use the Random and the Sound.speak method to either say
         // "Simon says press this key" or "Press this key"
-
+        int x = ran.nextInt(2);
+        if(x == 0) {
+        	simonSays = true;
+        	Sound.speak("Simon says press this key");
+        }else if(x == 1) {
+        	simonSays = false;
+        	Sound.speak("Press this key");
+        }
         // 14. Above, set the value of simonSays to true/false appropriately
 
     }
